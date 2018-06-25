0864ccb6c91b1909146925643ffd308f019d74dc
jmoypace <jmoypace@gmail.com>
2018-06-25 14:37:14 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,9 +2,9 @@ def oxford_comma(array)
   if(array.length >2)
     array.join(", ")
     array.split(" ")
-    lastword=array.pop
-    array.push("and")
-    array.push(lastword)
+  #  lastword=array.pop
+    #array.push("and")
+  #  array.push(lastword)
     array.join
 else
 
