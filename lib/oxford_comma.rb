60100a53000e054f5ade668cd52482d548a2def6
jmoypace <jmoypace@gmail.com>
2018-06-25 14:19:33 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,9 +1,10 @@
 def oxford_comma(array)
   if(array.length >1)
-    #anda=array.pop
-    #array.push(" and ")
-    #array=array.push(anda)
+    anda=array.pop
+    array.push(" and ")
+    array=array.push(anda)
     array.join(" ")
+
   end
 else
 array.join(" and ")
